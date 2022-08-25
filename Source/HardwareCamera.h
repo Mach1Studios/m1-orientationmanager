e602d8402f4ccae126cda3057df0c28d075a5432
Dylan Marcus <dylan@mach1studios.com>
2022-08-25 22:58:47 +0100

    updated jucer

---
+++ Source/HardwareCamera.h
@@ -0,0 +1,11 @@
+//
+//  M1-OrientationManager
+//  Copyright © 2022 Mach1. All rights reserved.
+//
+
+#pragma onec
+
+#include <JuceHeader.h>
+#include "HardwareAbstract.h"
+//"#include" Devices/mediapipe/mediapipe/examples/desktop"simple_run_graph_main.cc
+
