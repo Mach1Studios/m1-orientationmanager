0620e261b22dc3d4986a7ae31ba78d99d9771d83
andorxornot <andorxornot@gmail.com>
2022-08-25 23:55:38 +0300

    added osc devices stub

---
+++ Source/HardwareOSC.h
@@ -0,0 +1,72 @@
+//
+//  M1-OrientationManager
+//  Copyright © 2022 Mach1. All rights reserved.
+//
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "HardwareAbstract.h"
+
+class HardwareOSC : public HardwareAbstract {
+public:
+    Orientation orientation;
+    M1OrientationDeviceInfo connectedDevice;
+    std::vector<M1OrientationDeviceInfo> devices;
+    bool isConnected = false;
+
+    void setup() override {
+        refreshDevices();
+    }
+
+    void update() override {
+        if (isConnected){
+            // generate random angles
+            float yaw = 180 * sin(juce::Time::currentTimeMillis() / 100000.0);
+            float pitch = 180 * cos(juce::Time::currentTimeMillis() / 100000.0 - 0.3);
+            float roll = 180 * sin(juce::Time::currentTimeMillis() / 100000.0 + 0.1);
+
+            orientation.setYPR({ yaw, pitch, roll });
+        }
+    }
+    
+    void close() override {
+        isConnected = false;
+    }
+
+    M1OrientationTrackingResult getOrientation() override {
+        M1OrientationTrackingResult result;
+        result.currentOrientation = orientation;
+        result.success = true;
+        return result;
+    }
+
+    void refreshDevices() override {
+        devices.push_back({ "test 1", M1OrientationDeviceType::M1OrientationManagerDeviceTypeSerial, "test 1" });
+        devices.push_back({ "test 2", M1OrientationDeviceType::M1OrientationManagerDeviceTypeSerial, "test 2" });
+    }
+
+    std::vector<M1OrientationDeviceInfo> getDevices() override {
+        return devices;
+    }
+
+    void startTrackingUsingDevice(M1OrientationDeviceInfo device, std::function<void(bool success, std::string errorMessage)> statusCallback) override {
+        auto matchedDevice = std::find_if(devices.begin(), devices.end(), M1OrientationDeviceInfo::find_id(device.getDeviceName()));
+        if (matchedDevice != devices.end()) {
+            // todo com port
+            int comPort = std::stoi(matchedDevice->getDeviceAddress());
+            connectedDevice = *matchedDevice;
+
+            isConnected = true;
+
+            statusCallback(true, "ok");
+            return;
+        }
+        statusCallback(false, "not found");
+    }
+
+    M1OrientationDeviceInfo getConnectedDevice() override {
+        return connectedDevice;
+    }
+
+};
