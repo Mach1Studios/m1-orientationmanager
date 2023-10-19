0d15db8d25954a5e92442c5d0d80e339426966ea
Dylan Marcus <dylan@dylanjmarcus.com>
2023-10-19 18:54:54 -0400

    add a quit command for any instance started after the 1st

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -60,6 +60,7 @@ public:
         // When another instance of the app is launched while this one is running,
         // this method is invoked, and the commandLine parameter tells you what
         // the other instance's command-line arguments were.
+        quit();
     }
 
     //==============================================================================
