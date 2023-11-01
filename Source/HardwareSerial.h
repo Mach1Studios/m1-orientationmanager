e955ba29392de974a8d9add6da88c3dda50e1f88
Dylan Marcus <dylan@dylanjmarcus.com>
2023-11-01 11:30:33 -0400

    bugfix of a missing close bracket

--- Source/HardwareSerial.h
+++ Source/HardwareSerial.h
@@ -199,8 +199,9 @@ public:
     }
     
     void calibrateDevice() override {
-        if (getConnectedDevice().getDeviceName().find("Supperware HT IMU") != std::string::npos && supperwareInterface.getTrackerDriver().isConnected()) {
+        if ((getConnectedDevice().getDeviceName().find("Supperware HT IMU") != std::string::npos) && supperwareInterface.getTrackerDriver().isConnected()) {
             supperwareInterface.calibrateCompass();
+        }
     }
     
     int close() override {
